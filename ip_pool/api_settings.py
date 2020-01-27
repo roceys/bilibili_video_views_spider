52f632551ce91f4a0569171adf7e7929dad61c9e
Alicinya <283438692@qq.com>
2020-01-27 19:43:07 +0800

    更新ip池

--- ip_pool/api_settings.py
+++ ip_pool/api_settings.py
@@ -3,5 +3,5 @@ TIME_OUT = 4
 URL_MAIN = 'https://www.xicidaili.com/nn/{}.html'
 URL_TEST = 'http://httpbin.org/get'
 FILE_NAME = os.path.abspath('ip_pool/proxies.csv')
-USE_PROXY = False
+USE_PROXY_TO_XICI = False
 THREAD_DELTA = 1
