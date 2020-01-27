4a1f1740111d4b9fa165b00c7d938769b747f2df
Alicinya <283438692@qq.com>
2020-01-27 22:10:18 +0800

    更新ip池

--- manage.py
+++ manage.py
@@ -1,9 +1,4 @@
 import browser_driver as bd
-from ip_pool.get_proxies import ProxiesSpider
 
-count = 1
-while True:
-    bd.loop_ip_play()
-    if count % 100 == 0:
-        ProxiesSpider().run()
-    count +=1
+bd.loop_ip_play()
+
