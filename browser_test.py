9417c190b19d67c98e3ed6d44b9a1b698f51c635
Alicinya <283438692@qq.com>
2020-02-02 22:14:48 +0800

    获取视频播放次数

--- browser_test.py
+++ browser_test.py
@@ -1,22 +1,27 @@
 import time
-
-from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-
-proxy = '117.90.4.159:9999'
-ops = Options()
-# ops.add_argument('--headless')
-# ops.add_argument('--disable-dev-shm-usage')
-# ops.add_argument('--disable-gpu')
-# ops.add_argument('--user-agent=%s' % ua)
-ops.add_argument('--no-sandbox')
-ops.add_argument('--proxy-server=http://%s' % proxy)
-
-# driver = webdriver.Chrome(chrome_options=ops)
-with webdriver.Chrome(chrome_options=ops) as driver:
-    driver.delete_all_cookies()
-    print('--proxy-server=http://%s' % proxy)
-    driver.get("http://httpbin.org/get")
-    time.sleep(10)
+from selenium import webdriver
 
 
+def test1():
+    proxy = '117.90.4.159:9999'
+    ops = Options()
+    # ops.add_argument('--headless')
+    # ops.add_argument('--disable-dev-shm-usage')
+    # ops.add_argument('--disable-gpu')
+    # ops.add_argument('--user-agent=%s' % ua)
+    ops.add_argument('--no-sandbox')
+    ops.add_argument('--proxy-server=http://%s' % proxy)
+    # driver = webdriver.Chrome(chrome_options=ops)
+    with webdriver.Chrome(chrome_options=ops) as driver:
+        driver.delete_all_cookies()
+        print('--proxy-server=http://%s' % proxy)
+        driver.get("http://httpbin.org/get")
+        time.sleep(10)
+
+
+
+
+
+if __name__ == '__main__':
+    ...
---
+++ utils.py
@@ -0,0 +1,23 @@
+import re
+import requests
+
+
+def get_bilibili_play_times(url):
+    # url = 'https://www.bilibili.com/video/av20379071/'
+    headers = {
+        # 'Accept': 'application/json, text/plain, */*',
+        # 'Accept-Encoding': 'gzip, deflate, br',
+        # 'Accept-Language': 'zh-CN,zh;q=0.9',
+        # 'Cache-Control': 'no-cache',
+        # 'Connection': 'keep-alive',
+        # 'Host': 'api.bilibili.com',
+        # 'Origin': 'https://space.bilibili.com',
+        # 'Pragma': 'no-cache',
+        # 'Referer': 'https://space.bilibili.com/10241912/video',
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
+    }
+    res = requests.get(url, headers=headers)
+    re_str = '"viewseo":(\d*)'
+    result = re.findall(re_str, res.text)
+    times = result[0] if result else None
+    return times
